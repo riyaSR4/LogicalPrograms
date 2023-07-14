9bbdd3950b8c7a675e939ef2677b527432ef7b22
Riya Susan Reji <riyasusan.r@tvsnext.com>
2023-07-14 15:13:32 +0530

    [Riya] Add : Implemented Fibonacci Series

--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace LogicalPrograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Enter input to be executed\n 1.Fibonacci Series\n 2.Exit\n");
+                int input = Convert.ToInt32(Console.ReadLine());
+                switch (input)
+                {
+                    case 1:
+                        Fibonacci fibonacci = new Fibonacci();
+                        fibonacci.FibonacciSeries();
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+
+                }
+            }
+        }
+    }
+}
